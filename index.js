b72e674f2de26c34226ba4eff92a4d1abcd57767
unsteelix <unsteelix@yandex.ru>
2022-11-24 16:52:28 +0900

    new domain

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-var http = require('http')
+let proxy = require('express-http-proxy')
+var express = require("express");
+var app = express();
 
-var httpProxy = require('http-proxy');
+app.use('/', proxy('http://picola2.unsteelix.keenetic.link/'))
+//app.listen(8008);
+module.exports = app;
+
+
+// var http = require('http')
+
+// var httpProxy = require('http-proxy');
  
-var proxy = httpProxy.createProxyServer(); 
+// var proxy = httpProxy.createProxyServer(); 
 
-http.createServer(function(req, res) {
-    proxy.web(req, res, { 
-        changeOrigin: true,
-        target: 'https://blog.unsteelix.keenetic.link/' 
-    });
-}).listen(8008);
+// http.createServer(function(req, res) {
+//     proxy.web(req, res, { 
+//         changeOrigin: true,
+//         target: 'https://blog.unsteelix.keenetic.link/' 
+//     });
+// }).listen(8008);
 
-proxy.on('error', function(e) {
-  console.log(e)
-});
+// proxy.on('error', function(e) {
+//   console.log(e)
+// });
